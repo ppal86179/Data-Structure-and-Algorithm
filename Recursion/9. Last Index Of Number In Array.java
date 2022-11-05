9b5dea276f025caa056e1e1983ed9419a5bd8b9a
Pradeep Pal <72936744+ppal86179@users.noreply.github.com>
2022-11-05 10:37:12 +0530

    Last Index Of Number In Array

--- Recursion/9. Last Index Of Number In Array.java
+++ Recursion/9. Last Index Of Number In Array.java
@@ -1,35 +1,46 @@
-//Eclipse-->Recursion
+package Recursion;
 
-class Solution{
-   public static int lastIndex(int input[],int x)
+public class Find_LastIndex_Of_Number {
+
+	public static int lastIndex(int input[],int x)
 	{
 	     return lastIndex(input,x,0);	
 	}
+	
 	private static int lastIndex(int input[], int x, int startIndex)
 	{
-		//BASE CASE
-		if(startIndex==input.length-1){
-		     if(input[startIndex]==x){
+		
+		if(startIndex==input.length-1)
+		{
+		     if(input[startIndex]==x)
+		     {
 		       return startIndex;
 		     }
-		     else{
+		     else
+		     {
+		    	// System.out.println("1. -1");
 						return -1;
 		     }
 		}
-    
-    //Recursion
 		int ans = lastIndex(input,x,startIndex+1);
-    
-    //our work --> if recursion will not give -1 that means we found last index return that index
-    //but if recursion give -1 as ans then we need to check our first element
-		if(ans1=-1){
+		if(ans!=-1)
+		{
 			return ans;
 		}
-		else{
+		else
+		{
 			if(input[startIndex]==x)
 				return startIndex;
 			else
+			//	System.out.println("2. -1");
 				return -1;
 		}
 	}
+	
+	public static void main(String[] args) {
+	
+		int input[] = {1,3,5,4,6};
+		System.out.println(lastIndex(input,5));
+	}
+
 }
