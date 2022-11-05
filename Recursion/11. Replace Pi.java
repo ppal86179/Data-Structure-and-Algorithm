cf6b907dae493c2e52afccb28de83a82d4c92a31
Pradeep Pal <72936744+ppal86179@users.noreply.github.com>
2022-11-05 10:47:40 +0530

    Replace Pi

--- Recursion/11. Replace Pi.java
+++ Recursion/11. Replace Pi.java
@@ -1,20 +1,26 @@
-//Eclipse --> recursion
+package Recursion;
 
-class Solution{
-  public static String replace(String input)
+public class Replace_Pi {
+
+	public static String replace(String input)
 	{
-  //BASE CASE:- if only single character is there return that character
+		//BASE CASE:- if only single character is there return that character
 		if(input.length()<=1)
 			return input.substring(0);
 		
-  // call recursion from 1st index till last index  
+// call recursion from 1st index till last index  
 		String temp = replace(input.substring(1));
-    
-  //check given string first char is 'p' or not if it is 'p' than check for recursion string ans first char is 'i' or not  
+		
+//check given string first char is 'p' or not if it is 'p' than check for recursion string ans first char is 'i' or not  
 		if(temp.charAt(0)=='i' && input.charAt(0)=='p')
 		{
 			return "3.14"+temp.substring(1);
 		}
 		return input.charAt(0)+temp.substring(0);
 	}
+	
+	public static void main(String[] args) {
+		System.out.println(replace("pipix"));
+	}
+
 }
