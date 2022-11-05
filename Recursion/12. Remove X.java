d08623b0a414ee456d2510574a7a4b6e5333bbfb
Pradeep Pal <72936744+ppal86179@users.noreply.github.com>
2022-11-05 10:50:46 +0530

    Remove X

--- Recursion/12. Remove X.java
+++ Recursion/12. Remove X.java
@@ -1,10 +1,13 @@
-//Eclipse-->recursion
+package Recursion;
 
-class Solution{
-  public static String removeX(String input)
+public class Remove_X {
+
+	public static String removeX(String input)
 	{
-		if(input.length()<=1){
-			if(input.charAt(0)=='x'){
+		if(input.length()<=1)
+		{
+			if(input.charAt(0)=='x')
+			{
 				return "";
 			}
 			else
@@ -13,9 +16,16 @@ class Solution{
 		
 		String temp = removeX(input.substring(1));
 		
-		if(input.charAt(0)=='x'){
+		if(input.charAt(0)=='x')
+		{
 			return temp;
 		}
 		return input.charAt(0)+temp.substring(0);
 	}
+	
+	public static void main(String[] args) {
+		System.out.println(removeX("pradxxxeepx"));
+	}
+
 }
+OUTPUT :- pradeep
