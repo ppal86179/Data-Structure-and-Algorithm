7db43b614c02b9e2e5d4f49cf75b51f1e16de208
Pradeep Pal <72936744+ppal86179@users.noreply.github.com>
2022-11-05 10:30:07 +0530

    First Index Of Number In Array

--- Recursion/8. First Index Of Number In Array.java
+++ Recursion/8. First Index Of Number In Array.java
@@ -1,14 +1,15 @@
-//Eclipse-->Recursion
+package Recursion;
 
-class Solution{
-  public static int firstIndex(int input[], int x)
+public class Find_FirstIndex_Of_Number {
+
+	public static int firstIndex(int input[], int x)
 	{
 		return firstIndex(input,x,0);
 	}
 	
 	private static int firstIndex(int input[] , int x, int startIndex)
 	{
-  //BASE CASE--> if only one element is there check it is equal to x or not
+		 //BASE CASE--> if only one element is there check it is equal to x or not
 			if(startIndex>=input.length-1)
 			{
 				if(input[startIndex]==x)
@@ -18,17 +19,24 @@ class Solution{
 				else
 					return -1;
 			}
-      
-  //Recursion--> recursion will give their ans
+			
+			
+			//Recursion--> recursion will give their ans
 			int ans = firstIndex(input,x,startIndex+1);
-      
-      
-  //our work --> check first element if it is equal to x return that index else return recursion ans    
+			
+
+			//our work --> check first element if it is equal to x return that index else return recursion ans 
 			if(input[startIndex]==x)
 			{
 				return startIndex;
 			}
 			else
 			return ans;
+	}
+	public static void main(String[] args)
+	{
+		int input[] = {1,2,3,4,5};
+		System.out.println(firstIndex(input,5));
+		System.out.println(firstIndex(input,7));
 	}
 }
