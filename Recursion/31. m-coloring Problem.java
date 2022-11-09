c349002f7eee95cf03512292fbe0c35ac69dc4be
Pradeep Pal <72936744+ppal86179@users.noreply.github.com>
2022-11-09 20:56:24 +0530

    Leetcode

--- Recursion/31. m-coloring Problem.java
+++ Recursion/31. m-coloring Problem.java
@@ -1,29 +1,29 @@
 class solve {
     public boolean graphColoring(boolean graph[][], int m, int n) {
-        int color[] = new int[n];
+        int Nodecolor[] = new int[n];
         for(int i=0;i<n;i++)
-           color[i]=-1;
-        return help(0,graph,m,n,color);
+           Nodecolor[i]=-1;
+        return help(0,graph,m,n,Nodecolor);
     }
-    public boolean help(int curr,boolean graph[][],int m,int n,int color[]){
+    public boolean help(int curr,boolean graph[][],int m,int n,int Nodecolor[]){
         
         if(curr==n)return true;
         
         for(int i=0; i<m; i++){
-            if(checkColor(graph,color,curr,i)){
-                color[curr] = i;
-                if(help(curr+1,graph,m,n,color))
+            if(checkColor(graph,Nodecolor,curr,i)){
+                Nodecolor[curr] = i;
+                if(help(curr+1,graph,m,n,Nodecolor))
                 return true;
                 
-                color[curr] = -1;
+                Nodecolor[curr] = -1;
             }
         }
         return false;
     }
-    public boolean checkColor(boolean[][] graph,int color[],int curr,int i){
+    public boolean checkColor(boolean[][] graph,int Nodecolor[],int curr,int i){
         for(int j=0; j<graph.length; j++){
             if(graph[curr][j]){
-                if(color[j]==i)
+                if(Nodecolor[j]==i)
                 return false;
             }
         }
