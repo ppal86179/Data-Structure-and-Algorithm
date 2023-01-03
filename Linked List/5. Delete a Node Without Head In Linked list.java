1852b5763a59c443cb0a458a0a3142c243825092
Pradeep Pal <72936744+ppal86179@users.noreply.github.com>
2023-01-03 22:07:12 +0530

    GFG

---
+++ Linked List/5. Delete a Node Without Head In Linked list.java
@@ -0,0 +1,24 @@
+/*
+class Node
+{
+	int data ;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+
+//Function to delete a node without any reference to head pointer.
+class Solution
+{
+    void deleteNode(Node del)
+    {
+        //1. Swap data
+         del.data = del.next.data;
+        //2. create link
+         del.next = del.next.next;
+    }
+}
